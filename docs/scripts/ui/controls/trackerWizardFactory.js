d646caf8fdde520bc5fde1290abaa4f21eecfe88
Mme Desktop <Mme@Desktop.com>
2026-07-16 07:46:15 +0200

    - Selecatbel Monster Icons and Color implemented

--- docs/scripts/ui/controls/trackerWizardFactory.js
+++ docs/scripts/ui/controls/trackerWizardFactory.js
@@ -7,7 +7,7 @@ export function createTrackerWizard() {
         <dialog id="trackerWizard" class="tracker-wizard">
             <div id="imageSelectContainer" class="image-select-container">
                 <div class="imageSelectorRow">
-                    <img class="monster-icon" src="img/monster-icon-Goblin.png" data-image="img/monster-icon-Goblin.png">
+                    <img class="monster-icon selected" src="img/monster-icon-Goblin.png" data-image="img/monster-icon-Goblin.png">
                     <img class="monster-icon" src="img/monster-icon-Orc.png" data-image="img/monster-icon-Orc.png">
                     <img class="monster-icon" src="img/monster-icon-ChaosWarrior.png" data-image="img/monster-icon-ChaosWarrior.png">
                 </div>
@@ -23,7 +23,7 @@ export function createTrackerWizard() {
                 </div>
             </div>
             <div id="colorSelectContainer" class="color-select-container">
-                <div class="color-option" data-color="#FF0000" style="background-color: #FF0000;"></div>
+                <div class="color-option selected" data-color="#FF0000" style="background-color: #FF0000;"></div>
                 <div class="color-option" data-color="#0084ff" style="background-color: #0084ff;"></div>
                 <div class="color-option" data-color="#002900" style="background-color: #002900;"></div>
                 <div class="color-option" data-color="#ff83f5" style="background-color: #ff83f5;"></div>
@@ -65,6 +65,16 @@ export function createTrackerWizard() {
 
 
     element.style.backgroundColor = "lightgray";
+    // Refresh the dialog each time it is clicked to ensure the selected image and color are reset
+    element.addEventListener('click', () => {
+        console.log('Tracker Wizard clicked');
+        // Get Selected Color from Colorselection 
+        let selectedColor = element.querySelector('.color-option.selected').getAttribute('data-color');
+        // Set Background of the selected image to the selected color
+        element.querySelectorAll('.monster-icon').forEach(i => i.style.backgroundColor =  "transparent"); // Reset all images to transparent
+        let selectedImage = element.querySelector('.monster-icon.selected');
+        selectedImage.style.backgroundColor = selectedColor;
+    });
     element.querySelector('#wizard-cancel-btn').addEventListener('click', () => element.close());
     element.querySelector('#wizard-create-btn').addEventListener('click', () => {
         const name = element.querySelector('#wizard-name-input').value;
@@ -73,13 +83,25 @@ export function createTrackerWizard() {
         const armor = parseInt(element.querySelector('#wizard-armor-input').value, 10);
         const focus = parseInt(element.querySelector('#wizard-focuspoints-input').value, 10);
         const move = parseInt(element.querySelector('#wizard-movingpoints-input').value, 10);
-
         const newMonsterTrackerEntity = new MonsterTrackerEntity(null, name, health, armor, focus, move);
-
         dbMonsterService.addMonsterTrackerEntity(newMonsterTrackerEntity);
         element.close();
-
     });
+    element.querySelectorAll('.monster-icon').forEach(icon => {
+        icon.addEventListener('click', () => {
+            element.querySelectorAll('.monster-icon').forEach(i => i.classList.remove('selected'));
+            icon.classList.add('selected');
+        });
+    });
+    element.querySelectorAll('.color-option').forEach(colorOption => {
+        colorOption.addEventListener('click', () => {
+            element.querySelectorAll('.color-option').forEach(c => c.classList.remove('selected'));
+            colorOption.classList.add('selected');
+            const selectedColor = colorOption.getAttribute('data-color');
+            //element.style.backgroundColor = selectedColor;
+        });
+    });
+
 
     return element;
 }
